164e80be426a5c882db82f49ac69b12df7e10988
huxdhxa <hux@vajrasecurity.cn>
2017-08-15 17:20:16 +0800

    pom spring

--- microservice/src/main/java/com/dhxa/bizcloud/microservice/App.java
+++ microservice/src/main/java/com/dhxa/bizcloud/microservice/App.java
@@ -1,9 +1,10 @@
 package com.dhxa.bizcloud.microservice;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/pubCode")
 public class App 
 {
     public static void main( String[] args )
