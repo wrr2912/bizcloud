b8ea4d4b2200ac3a2eec30ef5945fa4c8dbacbf8
huxdhxa <hux@vajrasecurity.cn>
2017-08-16 09:58:29 +0800

    jpa

--- microservice/src/main/java/com/dhxa/bizcloud/microservice/App.java
+++ microservice/src/main/java/com/dhxa/bizcloud/microservice/App.java
@@ -1,14 +1,16 @@
 package com.dhxa.bizcloud.microservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "/pubCode")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
     }
 }
+
