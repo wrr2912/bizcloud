6382b28d022a961918c7073279c5acc5f3a4fae4
huxdhxa <hux@vajrasecurity.cn>
2017-08-17 10:23:33 +0800

    eureka done

---
+++ appservice/src/main/java/com/dhxa/bizcloud/appservice/client/UserClient.java
@@ -0,0 +1,17 @@
+package com.dhxa.bizcloud.appservice.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.dhxa.bizcloud.appservice.entity.User;
+
+@FeignClient("MICRO-SERVICE")
+public interface UserClient {
+
+    @RequestMapping(value="/user/findUserByUserName", method=RequestMethod.GET)
+    public User findUserByUserName(@RequestParam("userName") String userName);
+
+}
