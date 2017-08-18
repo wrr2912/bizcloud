327c3f939d3108074f2828679270976adf60f126
wrr2912 <free2912521@sina.com>
2017-08-18 11:13:17 +0800

    dept management

--- microservice/src/main/java/com/dhxa/bizcloud/microservice/MicroServiceApp.java
+++ microservice/src/main/java/com/dhxa/bizcloud/microservice/MicroServiceApp.java
@@ -3,9 +3,6 @@ package com.dhxa.bizcloud.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
--- microservice/src/main/java/com/dhxa/bizcloud/microservice/controller/UserController.java
+++ microservice/src/main/java/com/dhxa/bizcloud/microservice/controller/UserController.java
@@ -1,18 +1,17 @@
 package com.dhxa.bizcloud.microservice.controller;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import com.dhxa.bizcloud.microservice.repository.IUserRepository;
 import com.dhxa.bizcloud.microservice.service.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
 	
-	private Logger log = Logger.getLogger(UserController.class);
+	private Logger log = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private UserService userService;
