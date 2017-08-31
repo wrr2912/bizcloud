4709a273d127ff57eb0ed178fba1266f64c5339d
haidongdhxa <958818740@qq.com>
2017-08-31 10:19:36 +0800

    merge

--- frontpage/src/utils/menu.js
+++ frontpage/src/utils/menu.js
@@ -1,6 +1,5 @@
 ﻿module.exports = [
   {
-<<<<<<< HEAD
     key: '6',
     name: '用户',
     icon: 'user',
@@ -15,15 +14,11 @@
     visible: false,
   },
   {
-    key: '7',
-    name: '单位',
-=======
     key: '1',
     name: '主页',
->>>>>>> bee63d70210947e4addec0b5b82891f312b507a6
     icon: 'home',
     link: '/dashboard',
-    visible: true,
+    visible: false,
   },
   {
     key: '2',
