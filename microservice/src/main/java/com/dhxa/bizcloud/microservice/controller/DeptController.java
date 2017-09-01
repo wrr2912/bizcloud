71e9c2276bae72f2dd861005560312d70b08268c
wrr2912 <free2912521@sina.com>
2017-09-01 10:27:49 +0800

    dept swagger-ui

--- microservice/src/main/java/com/dhxa/bizcloud/microservice/controller/DeptController.java
+++ microservice/src/main/java/com/dhxa/bizcloud/microservice/controller/DeptController.java
@@ -2,9 +2,14 @@ package com.dhxa.bizcloud.microservice.controller;
 
 import com.dhxa.bizcloud.microservice.entity.Dept;
 import com.dhxa.bizcloud.microservice.message.SystemErrorCodeType;
+import com.dhxa.bizcloud.microservice.message.SystemMessage;
 import com.dhxa.bizcloud.microservice.service.DeptService;
 import com.dhxa.bizcloud.microservice.utils.ResponseUtil;
 import com.rayfay.bizcloud.core.commons.exception.NRAPException;
+
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +26,12 @@ import java.util.List;
 public class DeptController {
     private Logger logger = LoggerFactory.getLogger(DeptController.class);
 
+    @ApiOperation(value=SystemMessage.M_DEPT_FIND_PAGE_BY_QUERY, notes=SystemMessage.M_DEPT_FIND_PAGE_BY_QUERY)
+    @ApiImplicitParams(value = {
+    		@ApiImplicitParam(name = SystemMessage.E_PAGE_SIZE, value = SystemMessage.C_PAGE_SIZE, required = true, paramType = "query", dataType = "int"),
+    		@ApiImplicitParam(name = SystemMessage.E_PAGE_NUM, value = SystemMessage.C_PAGE_NUM, required = true, paramType = "query", dataType = "int"),
+	        @ApiImplicitParam(name = SystemMessage.E_DEPT_NAME, value = SystemMessage.C_DEPT_NAME, required = false, paramType = "query", dataType = "String")
+	})
     @RequestMapping(value = "getPageable", method = RequestMethod.GET)
     @CrossOrigin
     public Object getDeptsPageable(@RequestParam(name = "pageSize", defaultValue = "10") int pageSize,
@@ -34,12 +45,15 @@ public class DeptController {
             logger.error(e.getMessage());
             throw new NRAPException(SystemErrorCodeType.E_GET_DATA_FALED);
         }
-        // return apiClient.getDeptPageable(pageSize,pageNumber,name);
     }
 
     @Autowired
     private DeptService deptService;
 
+    @ApiOperation(value=SystemMessage.M_DEPT_FIND_BY_ID, notes=SystemMessage.M_DEPT_FIND_BY_ID)
+    @ApiImplicitParams(value= {
+    		@ApiImplicitParam(name = SystemMessage.E_DEPT_ID, value=SystemMessage.C_DEPT_ID, required = true, paramType="query", dataType="Long")
+    })
     @RequestMapping(value = "get" , method = RequestMethod.GET)
     @CrossOrigin
     public Object getDept(@RequestParam Long id){
@@ -58,6 +72,7 @@ public class DeptController {
         }
     }
 
+    @ApiOperation(value = SystemMessage.M_DEPT_CREATE, notes = SystemMessage.M_DEPT_CREATE)
     @RequestMapping(value = "add", method = RequestMethod.POST)
     @Transactional
     @CrossOrigin
@@ -69,6 +84,8 @@ public class DeptController {
             throw new NRAPException(SystemErrorCodeType.E_ACTION_FALED,e.getMessage()+"增加");
         }
     }
+    
+    @ApiOperation(value = SystemMessage.M_DEPT_UPDATE, notes = SystemMessage.M_DEPT_UPDATE)
     @RequestMapping(value = "update", method = RequestMethod.PUT)
     @CrossOrigin
     public Object updateDept(@RequestBody Dept data) {
@@ -91,6 +108,10 @@ public class DeptController {
         }
     }
 
+    @ApiOperation(value=SystemMessage.M_DEPT_DELETE, notes=SystemMessage.M_DEPT_DELETE)
+    @ApiImplicitParams(value= {
+    		@ApiImplicitParam(name = SystemMessage.E_DEPT_ID, value=SystemMessage.C_DEPT_ID, required = true, paramType="query", dataType="Long")
+    })
     @RequestMapping(value = "delete", method = RequestMethod.DELETE)
     @CrossOrigin
     public Object delDept(@RequestParam Long id) {
